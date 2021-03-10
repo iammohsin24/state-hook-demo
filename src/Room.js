684d2a5ddb0f8afeeac263c545ddeb9f287ef63b
Mohsin <mohsin.manzoor171@gmail.com>
2021-03-10 19:09:58 +0500

    added room.css

--- src/Room.js
+++ src/Room.js
@@ -1,16 +1,22 @@
 import React , { useState } from 'react';
-import './App.css';
+import './Room.css';
 
 function Room() {
     let [lit,setLit] = useState(false);
     let [age,setage] = useState(20);
+    let [temp,setTemp] = useState(72);
   return (
-    <div>
+    <div className={`room ${lit? 'lit': 'dark'}`}>
         Room is {lit ? 'lit' : 'dark'}
+        <br/>
+        Current Temperature = ${temp} &deg;F
         <br/>
         Age  = {age}
         <br/>
-        <button onClick={()=>setLit(!lit)}>Toogle Light</button><br/>
+        <button onClick={()=>setLit(lit=true)}>Toogle Light:ON</button>
+        <button onClick={()=>setLit(lit=false)}>Toogle Light:OFF</button><br/>
+        <button onClick={()=>setTemp(++temp)}>+</button>
+        <button onClick={()=>setTemp(--temp)}>-</button><br/>
         <button onClick={()=>setage(++age)}>Increase Age</button>
     </div>
   );
